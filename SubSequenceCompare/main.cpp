4d3e082ec2b1b93b2ce90196b8d1d983b052de7d
Aleksander Zdunek <halekun@gmail.com>
2018-01-25 16:12:20 +0100

    Detect recurring contigious subsequences. Added Makefile

--- SubSequenceCompare/main.cpp
+++ SubSequenceCompare/main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
+#include <string>
 
 typedef char element_type;
 
 int subsequenceSearch(const element_type* exemplar, const size_t exemplarLength, const element_type* sequence, const size_t sequenceLength);
 bool beginEqually(const element_type* seq1, const element_type* seq2, const size_t length);
+int detectInitialSubsequenceRepeat(const element_type* seq, const size_t seqLength);
+int detectRepeatingSubsequences(const element_type* seq, const size_t seqLength);
+std::string toString(const char* seq, size_t length);
 
-const char string[] = "This is a character sequence.";
-size_t stringLength = 29;
+//const char string[] = "This is a This character sequence.";
+const char string[] = "abcdefghijklmnopqrstuvwxyzABCDabcEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+//const char string[] = "abcdefghijklmnopqrstuvwxyz";
+size_t stringLength = sizeof(string) - 1;
 
 int main(int argc, char* argv[])
 {
 	std::cout << "SubSequence Compare" << std::endl;
-
-	const char ex[] = "is";
-	size_t exLength = 2;
-
-	int found = subsequenceSearch(ex, exLength, string, stringLength);
+	
+	int found = detectRepeatingSubsequences(string, stringLength);
 	
 	std::cout << "found: " << found << std::endl;
 
@@ -42,4 +45,42 @@ bool beginEqually(const element_type* seq1, const element_type* seq2, const size
 	}
 	
 	return true;
+}
+
+int detectInitialSubsequenceRepeat(const element_type* seq, const size_t seqLength)
+{
+    int count = 0;
+    
+    size_t minExemplarLength = 1;
+    size_t maxExemplarLength = seqLength/2;
+    
+    for (size_t i = minExemplarLength; i <= maxExemplarLength; i++)
+    {
+        std::cout << "Ex: " << toString(seq, i) << " ";
+        int found = subsequenceSearch(seq, i, seq+i, seqLength-i);
+        std::cout << ";found: " << found << std::endl;
+        count += found;
+    }
+    
+    return count;
+}
+
+int detectRepeatingSubsequences(const element_type* seq, const size_t seqLength)
+{
+	int count = 0;
+    for (size_t i = 0; i < seqLength; i++)
+    {
+    	count += detectInitialSubsequenceRepeat(seq+i, seqLength-i);
+    }
+    return count;
+}
+
+std::string toString(const char* seq, size_t length)
+{
+    std::string s;
+    for(int i = 0; i<length; i++)
+    {
+        s.push_back(seq[i]);
+    }
+    return s;
 }
